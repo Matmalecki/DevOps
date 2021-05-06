6fcf4ece363c7bf112372ca53d0fde6e38edc28b
Mateusz Małecki <mmateusz314@gmail.com>
2021-05-06 21:11:47 +0200

    Changed update function

--- myfrontend/src/App.js
+++ myfrontend/src/App.js
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 import './App.css';
 import Books from './Books.js'
-import BookForm from './BookForm.js'
 
 function App() {
   return (
     <div>
       <Books/>
-      <BookForm/>
     </div>
   );
 }
--- myfrontend/src/Books.js
+++ myfrontend/src/Books.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 
 const Books = (props) => {
     const [posts, setPosts] = useState([]);
+    const [id, setId] = useState(null);
+    const [title, setTitle] = useState(props.title);
+    const [author, setAuthor] = useState(props.author);
+
     useEffect(()=> {
         axios.get('api/book')
             .then(response => setPosts(response.data))
@@ -15,18 +19,40 @@ const Books = (props) => {
             .catch(error => console.log(error));
     };
 
-    const updatePost = (post) => {
-        axios.put(`api/book/${post.id}`, 
-        {id: post.id, name:post.title+"_updated", author:post.author+"_updated"})
-            .catch(error => console.log(error));
+
+    const handleSubmit = (event) => {
+        console.log(`Dane do wyslania ${title} oraz ${author}`);
+        if (id != null)
+        {
+            axios.put(`api/book/${id}`, {name:title, author:author})
+                .then(response => console.log(response))
+                .catch(error => console.log(error));
+        } else {
+            axios.post('api/', {name:title, author:author})
+                .then(response => console.log(response))
+                .catch(error => console.log(error)); 
+        }
+        setAuthor("");
+        setId(null);
+        setTitle("");
+        event.preventDefault();
+    }
+
+    const setUpdatedPost = (post) => {
+      setAuthor(post.author);
+      setTitle(post.title);
+      setId(post.id);  
     };
 
     return (<div> 
+                <input type='text' value={title} onChange={event=>setTitle(event.target.value)}/> <br/>
+                <input type='text' value={author} onChange={event=>setAuthor(event.target.value)}/> <br/>
+                <input type='submit' value='OK' onClick={handleSubmit}/>
                 {posts.map(post => (
                     <div key={post.id}>
                         {post.title}, {post.author}
                         <button onClick={() => deletePost(post.id)}>X</button>
-                        <button onClick={() => updatePost(post)}>Update</button>
+                        <button onClick={() => setUpdatedPost(post)}>Update</button>
                     </div>
                 ))}
             </div>
